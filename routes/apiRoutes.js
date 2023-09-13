04ad5c3c4154a8564c85ac85518e86a86dfda5dd
Molly Starnes <mstrnes2@gmail.com>
2023-09-13 14:06:09 -0500

    added routes to the file apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json'), 'utf-8'));
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json'), 'utf-8'));
+
+})
