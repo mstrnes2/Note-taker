963d10f206772681770057d2c75cf210b47612b1
Molly Starnes <mstrnes2@gmail.com>
2023-09-13 18:23:47 -0500

    added readme file

---
+++ README.md
@@ -0,0 +1,41 @@
+
+  # Note-taker
+
+  
+
+  ## Description
+
+  This project allows you to write and save notes. It uses Express.js to save and retrieve notes from a JSON file.
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  
+  - [Contributing](#contributing)
+  - [Test](#test)
+  - [Question](#questions)
+
+  ## Installation
+
+  N/A
+
+  ## Usage
+
+  It is used to write notes and be able to save them to look back at the notes you wrote ealier.  I also added a delete on the notes that you can delete notes that you do not need anymore.
+
+  
+
+  ## Contributing
+
+  GitHub
+
+  ## Test
+
+  N/A
+
+  ## Questions
+
+  If you have any questions, please email me at: mstrnes2@gmail.com
+
+  If you would like to see more of my work, my GitHub link is [mstrnes2](https://github.com/mstrnes2)
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/apiRoutes.js');
-//const html = require('./routes/htmlRoutes.js');
+const html = require('./routes/htmlRoutes.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,20 +10,12 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-//app.use('/', html);
-
 app.use(express.static('public'));
+app.use('/api', api);
+app.use('/', html);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
 
